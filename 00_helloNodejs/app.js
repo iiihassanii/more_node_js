55598ad6c40246cdefe921432b3668e1a268c24c
iiihassanii <hassanAbdelazim.max@gmail.com>
2024-12-22 21:29:23 +0300

    module and export

---
+++ 00_helloNodejs/app.js
@@ -0,0 +1,5 @@
+const name2 = import('module');
+console.log(name2);
+console.log('Hello node js');
+console.log(__dirname);
+console.log(__filename)
---
+++ 00_helloNodejs/module.js
@@ -0,0 +1,6 @@
+//local scope
+const name1 = 'hassan';
+
+//export
+const name2 = 'Hassan';
+module.exports = {name2};
