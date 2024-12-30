e85c21a00a0c44abd99f717708ca469ecf6c971e
iiihassanii <hassanAbdelazim.max@gmail.com>
2024-12-30 16:15:26 +0300

    update

--- 02_event_loops/app.js
+++ 02_event_loops/app.js
@@ -1,19 +1,22 @@
 const fs = require('fs');
 console.log('Start read task');
 
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        if (!fs.existsSync(path)) {
+            reject(new Error(`الملف غير موجود: ${path}`));
+            return;
+        }
 
-const getText = (pass) =>{
-    return new Promise((resolve, reject)=>{
-        fs.readFile(pass, 'utf-8', (err, data)=>{
-            err? reject(err):resolve(data);
-        })
-    })
+        fs.readFile(path, 'utf-8', (err, data) => {
+            err ? reject(err) : resolve(data);
+        });
+    });
 }
 
 getText('./app.js')
-.then((result)=>{ console.log(result)})
-.catch((err)=> {console.log(err);})
-
+    .then((result) => { console.log(result) })
+    .catch((err) => { console.log(err); })
 
 console.log('End first task');
 
